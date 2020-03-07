cbcd24904cdc4fffd1beb79b2bf12896744bd649
Pias Paul <34069239+paul-pias@users.noreply.github.com>
2020-03-08 02:40:53 +0600

    server modified for multiple camera support

--- app.py
+++ app.py
@@ -2,29 +2,26 @@
 from flask import Flask, redirect, url_for, request, render_template, Response
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
-from camera import Camera
-from object_detection import object_detection
+from camera import ObjectDetection
 
 app = Flask(__name__)
-camera = Camera(0)
 @app.route("/")
 def main():
-    return render_template("test.html")
+    return render_template("index.html")
 
 def gen(camera):
     while True:
-        frame,l,feedback = camera.get_frame()
+        frame = camera.main()
         if frame != "":
             yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-
-    return Response(gen(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+    id = 0
+    return Response(gen(ObjectDetection(id)), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
     # Serve the app with gevent
-    http_server = WSGIServer(('localhost', 5000), app)
-    http_server.serve_forever()
+    app.run(host='0.0.0.0', threaded=True, debug = True)
